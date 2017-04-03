ede7ab02ec264192efc3682320928eeaec289eef
yhoussin <yhoussin@sqli.com>
2017-04-03 17:23:28 +0200

    test

--- server/app.js
+++ server/app.js
@@ -5,9 +5,12 @@ const path = require('path');
 const app = express();
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
-
-app.use(express.static(path.resolve(__dirname,'..','build')));
-app.use(express.static(path.resolve(__dirname,'..','public')));
+if (process.env.NODE_ENV === 'production') {
+    console.log('prod');
+    app.use(express.static('client/build'));
+}else{
+    app.use(express.static(path.resolve(__dirname,'..','public')));
+}
 app.get('/songs.json', (req, res) => {
     res.json(require('./songs.json'))
 });
